6ad23c4d187232955e7ddb2247096872cebb82d0
Caleb Riordan <caleb2003riordan@gmail.com>
2025-05-15 10:33:43 +0200

    Add Form, Validator, Session, and Authenticator classes

---
+++ app/Http/Forms/Form.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Http\Forms;
+
+use Core\ValidationException;
+
+class Form
+{
+    protected $errors = [];
+    protected $attributes = [];
+
+    public static function validate($attributes)
+    {
+        $instance = new static($attributes);
+
+        return $instance->failed() ? $instance->throw() : $instance;
+    }
+
+    public function throw()
+    {
+        ValidationException::throw($this->errors, $this->attributes);
+    }
+
+    public function failed()
+    {
+        return (bool) count($this->errors);
+    }
+
+    public function errors()
+    {
+        return $this->errors;
+    }
+
+    public function error($field, $message)
+    {
+        $this->errors[$field] = $message;
+
+        return $this;
+    }
+
+    protected static function missingKeys(array $attributes, array $keys): array{
+        $missing = [];
+        foreach ($keys as $key){
+            if (!isset($attributes[$key])){
+                $missing[] = $key;
+            }
+        }
+
+        return $missing;
+    }
+}
--- app/Http/controllers/login/store.php
+++ app/Http/controllers/login/store.php
@@ -2,7 +2,16 @@
 
 // Get form $_POST values
 
+use Http\Forms\LoginForm;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
 // validate form input on server side
+$form = LoginForm::validate([
+    'email' => $email,
+    'password' => $password,
+]);
 
 // authenticate user
 
